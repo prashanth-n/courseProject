83219802b21a3b41093a5d165a92936aa4d57c0b
crazygeek94 <prashanth0763@yahoo.com>
2017-09-06 15:59:24 +0530

    Workin on recipe list component

--- src/app/receipes/receipe-list/receipe-list.component.ts
+++ src/app/receipes/receipe-list/receipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipes.model';
 
 @Component({
   selector: 'app-receipe-list',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./receipe-list.component.css']
 })
 export class ReceipeListComponent implements OnInit {
-
+  recipe: Recipe[] = [
+    // tslint:disable-next-line:max-line-length
+    new Recipe('Recipe Test', 'Recipe desc', 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Banjo_Shark_recipe.jpg/320px-Banjo_Shark_recipe.jpg'),
+    new Recipe('Recipe Test 1', 'Recipe desc', 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Banjo_Shark_recipe.jpg/320px-Banjo_Shark_recipe.jpg'),
+    // tslint:disable-next-line:max-line-length
+    new Recipe('Recipe Test 2', 'Recipe desc', 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Banjo_Shark_recipe.jpg/320px-Banjo_Shark_recipe.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
